91e2ce09ba387a0a9271be424fc416376f176737
k9712132 <k9712132@daum.net>
2022-12-29 22:00:19 +0900

    first commit

---
+++ src/main/java/com/example/blog/test/BlogControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+	
+	//http://localhost:8080/test/hello
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+}
